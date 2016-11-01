599984d428446a4200165ccdfd780a0a5e6a0087
Thiruch Rakthao <sax_inw_com@hotmail.com>
2016-11-01 17:10:00 +0700

    Icreate_navigation: Add function in navigation class

--- icreate-platform/icreate_navigation/include/navigation.h
+++ icreate-platform/icreate_navigation/include/navigation.h
@@ -1,8 +1,14 @@
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
 
+#include <fstream>
+
+    
+
 namespace icreate {
+    typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 class Navigation {
     public:
@@ -10,22 +16,37 @@ class Navigation {
 
         ~Navigation();
 
+        void setRobotTarget(move_base_msgs::MoveBaseGoal goal);
+
+        void setRobotTarget(int selected_point);
+
+        move_base_msgs::MoveBaseGoal getRobotTarget();
+
         void setRobotGoal(std::string frame_id);
 
         move_base_msgs::MoveBaseGoal getRobotGoal();
 
-        std::string doneRobotGoal(std::string robot_state, bool &finish);
+        std::string doneRobotGoal(std::string robot_state, int &mode);
 
-        std::string activeRobotGoal(std::string robot_state);
+        std::string failRobotGoal(std::string robot_state, bool &finish);
+
+        std::string activeRobotGoal(std::string robot_state, std::string state_req);
 
         void getFeedbackRobotGoal();
 
         void read_waypoint_constant();
 
+        void read_waypoint_file(std::string filename);
 
+        void displayWaypoints();
+
+        void getUserInput();
         
 
     private:
+        // Convert String To Int
+        int toint(std::string s); //The conversion function
+
 
     public:
         // Move base Goal
@@ -33,9 +54,19 @@ class Navigation {
 
         std::vector<move_base_msgs::MoveBaseGoal> targets;
         std::vector<move_base_msgs::MoveBaseGoal>::iterator target;
+        std::vector<std::string> target_name;
+
+        MoveBaseClient* ac();
+
+        // Navigation Mode
+        // 0 : Go to Specific Point
+        // 1 : Delivery and Come Back to This Place
+        // 2 : Execute The Memorized Sequence
+        int navigation_mode;
 
     private:
         // move_base_msgs::MoveBaseAction action;
+        int selected_point;
 };
 
 }
--- icreate-platform/icreate_navigation/src/navigation.cpp
+++ icreate-platform/icreate_navigation/src/navigation.cpp
@@ -3,11 +3,28 @@
 namespace icreate {
 
 Navigation::Navigation() {
-    // ac("move_base", true);
+    // Navigation::ac("move_base", true);
+    navigation_mode = -1;
 }
 
 Navigation::~Navigation() {
 
+}
+
+void Navigation::setRobotTarget(move_base_msgs::MoveBaseGoal goal) {
+    *target = goal;
+}
+
+void Navigation::setRobotTarget(int selected_point) {
+    Navigation::selected_point = selected_point;
+    // *target = targets[selected_point];
+    target = targets.begin() + selected_point;
+    ROS_WARN("SElect Point: %d %lf",selected_point, (*target).target_pose.pose.position.x);
+}
+
+move_base_msgs::MoveBaseGoal Navigation::getRobotTarget() {
+    ROS_WARN("get Robot Target: %d",selected_point);
+    return targets[selected_point];
 }
 
 void Navigation::setRobotGoal(std::string frame_id) {
@@ -23,37 +40,69 @@ void Navigation::setRobotGoal(std::string frame_id) {
     goal.target_pose.pose.orientation.w = target->target_pose.pose.orientation.w;
     std::cout << "[AGENT] SET NEW GOAL ! " << std::endl;
 
-
 }
 
 move_base_msgs::MoveBaseGoal Navigation::getRobotGoal() {
     return goal;
 }
 
-std::string Navigation::doneRobotGoal(std::string robot_state, bool &finish) {
+std::string Navigation::doneRobotGoal(std::string robot_state, int &mode) {
     std::string state_msg; 
     ROS_INFO("The goal was reached!");
     if(robot_state == "GOING") {
         state_msg = "WAITING";
-        // ----------------------------------- Not Implemented Now ------------------------ //
-        // waitfordelivery();
+        mode = 1;
+    }
+    else if(robot_state == "WAITING") {
+        
     }
     else if(robot_state == "BACKTOBASE") {
         state_msg = "IDLE";
-        finish = true;  
+        mode = 0;
+    }
+    else if(robot_state == "SINGLERUN") {
+        state_msg = "IDLE";
+        mode = 0;
+    }
+    else if(robot_state == "EXECUTESEQ") {
+        state_msg = "IDLE";
+        mode = 2;
     }
     return state_msg;
 }
 
-std::string Navigation::activeRobotGoal(std::string robot_state) {
+std::string Navigation::failRobotGoal(std::string robot_state, bool &finish) {
+    std::string state_msg;
+     if(robot_state == "GOING") {
+
+     }
+     else if(robot_state == "SINGLERUN") {
+
+     }
+     else if(robot_state == "EXECUTESEQ") {
+         
+     }
+    return state_msg;
+}
+
+std::string Navigation::activeRobotGoal(std::string robot_state, std::string state_req) {
     std::string msg;
     ROS_INFO("Goal active! Now !!");
-    if(robot_state == "IDLE") {
+    if(robot_state == "IDLE" && state_req == "SINGLERUN") {
+        msg = "SINGLERUN";
+    }
+    else if(robot_state == "IDLE" && state_req == "GOING") {
         msg = "GOING";
+    }
+    else if(robot_state == "IDLE" && state_req == "EXECUTESEQ") {
+        msg = "EXECUTESEQ";
     }
     else if(robot_state == "WAITING") {
         msg = "BACKTOBASE";
     }
+    // else {
+    //     msg = "IDLE";
+    // }
     return msg;
 }
 
@@ -63,15 +112,84 @@ void Navigation::getFeedbackRobotGoal() {
 
 void Navigation::read_waypoint_constant() {
     move_base_msgs::MoveBaseGoal newPoint;
-    newPoint.target_pose.pose.position.x    = -6.326;
-    newPoint.target_pose.pose.position.y    = -0.674;
+    newPoint.target_pose.pose.position.x    = 7.687;
+    newPoint.target_pose.pose.position.y    = 14.260;
     newPoint.target_pose.pose.orientation.x = 0.000;
     newPoint.target_pose.pose.orientation.y = 0.000;
-    newPoint.target_pose.pose.orientation.z = -2.870;
-    newPoint.target_pose.pose.orientation.w = 0.958;
+    newPoint.target_pose.pose.orientation.z = 0.552;
+    newPoint.target_pose.pose.orientation.w = 0.834;
     targets.push_back(newPoint);
 
     target = targets.begin();    
+}
+
+void Navigation::read_waypoint_file(std::string filename) {
+    std::vector<std::string> tokenized;
+    std::string path = ros::package::getPath("icreate_navigation")+ filename;
+    std::ifstream inFile(path.c_str());
+    std::string line;   
+    //Prompt to User
+    std::cout << "[POINT_READER]IMPORTING Point of interest !" <<std::endl;     
+    //First Value is Waypoint Counts 
+    getline(inFile,line);
+    int waypoint_count = toint(line);
+    std::cout << "[POINT_READER]Points Counted : " << waypoint_count <<std::endl;
+
+    //Read POI Line By Line 
+    while(getline(inFile,line)){
+     // New Line
+       std::stringstream strstr(line);
+       std::string word = "";
+     // Ignore First Param (Place's Name)
+       getline(strstr,word,',');
+       target_name.push_back(word);
+     // Gather Params
+       while(getline(strstr,word,',')){
+           tokenized.push_back(word);
+       }
+    }
+
+    //Count All Parameters
+    size_t counter = tokenized.size();
+    std::cout << "[POINT_READER]Counter  : " << counter <<std::endl; 
+    size_t point_amount = (size_t)(counter / 6.0);
+    std::cout << "[POINT_READER]Div Count : " << point_amount << " ,  File Count = " << waypoint_count <<std::endl;
+
+    std::vector<std::string>::iterator word_it;
+    word_it = tokenized.begin();
+
+    // Create move_base GOAL and Push into vector ! 
+    for(int point_index = 0  ; point_index < counter ; point_index++){
+       move_base_msgs::MoveBaseGoal newPoint;
+       newPoint.target_pose.pose.position.x    = std::atof((word_it++)->c_str());
+       newPoint.target_pose.pose.position.y    = std::atof((word_it++)->c_str());
+       newPoint.target_pose.pose.orientation.x = std::atof((word_it++)->c_str());
+       newPoint.target_pose.pose.orientation.y = std::atof((word_it++)->c_str());
+       newPoint.target_pose.pose.orientation.z = std::atof((word_it++)->c_str());
+       newPoint.target_pose.pose.orientation.w = std::atof((word_it++)->c_str());
+       targets.push_back(newPoint);
+       if(word_it == tokenized.end())break;
+    }
+
+    //Prompt End of Process
+    std::cout << "[POINT_READER] Point of Interest in imported" << std::endl;
+}
+
+// Display Waypoints
+void Navigation::displayWaypoints() {
+    for(int i = 0 ; i < target_name.size() ; i++) {
+      std::cout <<"["<<i<<"] " << target_name[i] << " " << targets[i].target_pose.pose.position.x <<std::endl; 
+    }
+}
+
+void Navigation::getUserInput() {
+
+}
+
+
+// Convert String To Int
+int Navigation::toint(std::string s) { //The conversion function 
+    return atoi(s.c_str());
 }
 
 }
