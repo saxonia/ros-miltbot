42895a0396e360966a7ff0522ffd23d3aa13e506
Thiruch Rakthao <sax_inw_com@hotmail.com>
2017-04-19 13:07:36 +0700

    Miltbot: add etc

--- universal-platform/miltbot_vision/src/door_detection.cpp
+++ universal-platform/miltbot_vision/src/door_detection.cpp
@@ -45,28 +45,28 @@ int main(int argc, char **argv)
     #ifdef KINECT_STREAM
         ROS_INFO("KINECT STREAM MODE");
         miltbot::MonoCamera cam;
-        // cam.createTrackbars();
+        cam.createTrackbars();
         ros::Rate rate(30);
         ROS_INFO("Before loop");
         while(ros::ok())
         {
             ros::spinOnce();
             rate.sleep();
-            // if(!cam.color_view.data || !cam.depth_view.data) {
-            //     continue;
-            // }
-            // cam.detectColorTape(cam.color_view);
+            if(!cam.color_view.data || !cam.depth_view.data) {
+                continue;
+            }
+            cam.detectColorTape(cam.color_view);
             // cv::namedWindow("color_img");
             // cv::namedWindow("depth_img");
             // cam.resizeImage(cam.color_view, 240, 240);
             // cam.resizeImage(cam.depth_view, 240, 240);
-            // cv::imshow("color_img",cam.color_view);
+            cv::imshow("color_img",cam.color_view);
             // cv::imshow("depth_img",cam.depth_view);
             // cv::imshow("depth_norm",cam.depth_norm);
-            // cv::imshow("color_tape",cam.color_tape_view);
-            // cv::imshow("color_tape_proc",cam.color_tape_proc);
-            // int key = cv::waitKey(30);
-            // if(key == 27) break;
+            cv::imshow("color_tape",cam.color_tape_view);
+            cv::imshow("color_tape_proc",cam.color_tape_proc);
+            int key = cv::waitKey(30);
+            if(key == 27) break;
         }
     #endif
     ROS_INFO("END");
